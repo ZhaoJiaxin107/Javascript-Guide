56c7e88bfaf4186a2547be45e886d5b6c539b5fb
Jiaxin Zhao <jzhao537@uwo.ca>
2021-10-28 22:19:34 +0800

    add concat and splice

---
+++ 07-Array/slice,splice,fill,copyWithIn.js
@@ -0,0 +1,48 @@
+// slice() 方法返回一个数组的切片或者子数组
+// 两个参数：返回参数的起止位置
+// 一个参数：返回的数组将包含从该起点直到数组末尾的所有元素
+// 参数负值：
+// 参数-1：数组的最后一个元素
+// 参数-2：数组的倒数第二个元素
+
+let a = [1, 2, 3, 4, 5];
+console.log(a.slice(0, 3)); // [1, 2, 3]
+console.log(a.slice(3)); // [4, 5]
+console.log(a.slice(1, -1)); // [2, 3, 4]
+console.log(a.slice(-3, -1)); // [3，4]
+
+
+// splice()
+// splice() 是一个对数组进行插入和删除的通用方法
+// splice() 可以从数组中删除元素, 可以向数组中插入新元素, 也可以同时执行这两种操作
+// splice() 的第一个参数指定指定插入或删除操作的起点位置，第二个参数指定要从数组中删除的元素个数
+// 省略第二个参数，从起点元素开始的所有数组元素都将被删除
+
+// splice() 返回被删除元素的数组, 如果没有删除元素则返回空数组
+let b = [1, 2, 3, 4, 5, 6, 7, 8];
+console.log(b.splice(4)); // [5, 6, 7, 8] 
+console.log(b); // [1, 2, 3, 4]
+console.log(b.splice(1, 2)); // [2, 3] 
+console.log(b); // [1, 4]
+console.log(b.splice(1, 1)); // [4]
+console.log(b); // [1]
+
+// splice(startIndex, length, item1, item2) 
+// 第一个参数的指定位置插入到数组中的元素
+let c = [1, 2, 3, 4, 5];
+console.log(c.splice(2, 0, "a", "b"));
+console.log(c); 
+/* [
+    1, 2, 'a', 'b',
+    3, 4, 5
+  ] 
+*/
+// splice() 会改变数组本身
+
+// concat(): 创建并返回一个新数组
+let d = [1, 2, 3];
+console.log(d.concat(4, 5, 6));// [1,2,3,4,5,6]
+console.log(d);
+
+
+
