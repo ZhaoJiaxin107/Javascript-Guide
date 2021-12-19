6c0df40c04b78883e3f44bbbb7a0034f5aa3ce90
Jiaxin Zhao <jzhao537@uwo.ca>
2021-12-19 15:05:30 +0800

    add vue project

---
+++ 21-vue/vue-base-project/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <ClickDisplay />
+  </div>
+</template>
+
+<script>
+// import TplDemo from './components/BaseUse/TplDemo.vue'
+import ClickDisplay from './components/BaseUse/ClickDisplay.vue'
+export default {
+  name: 'App',
+  components: {
+    ClickDisplay
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ 21-vue/vue-base-project/src/components/BaseUse/ClickDisplay.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <div v-if="isshow">{{isshow}}</div>
+        <button @click="change()">点击</button>
+    </div>
+</template>
+<script>
+export default({
+  name: '',
+  data () {
+    return {
+      isshow: false
+    }
+  },
+  methods: {
+    change: function () {
+      this.isshow = !this.isshow
+    }
+  }
+})
+</script>
